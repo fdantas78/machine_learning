aa2edcfa9ba96e3f54f867f38ecb59169f010c76
fernando <fernando@192.168.1.100>
2017-11-27 15:27:07 -0700

    testing installation of theano tensorflow and keras

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -9,4 +9,17 @@ import pandas
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-lenght', 'sepal-width', 'petal-lenght', 'petal-width','class']
 dataset = pandas.read_csv(url, names=names)
+print(dataset.shape)
+print(dataset.describe())
+print(dataset.groupby('class').size()) 
 
+#testing installation of theano tensorflow and keras
+
+import theano
+print('theano: %s' % theano.__version__)
+
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+
+import keras
+print('keras: %s' % keras.__version__)
